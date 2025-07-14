9a7daaade238ae95cdacb32435d1ae296750dc5d
Weslley Sniper <weslley.kacau@gmail.com>
2025-07-14 12:45:04 +0000

    mapbox : pk.eyJ1Ijoid2VzbGxleWF0aGlsYSIsImEiOiJjazlnNTY2eXUwaHo2M2tsYzZx

---
+++ src/next.config.ts
@@ -0,0 +1,29 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_MAPBOX_TOKEN: "pk.eyJ1Ijoid2VzbGxleWF0aGlsYSIsImEiOiJjazlnNTY2eXUwaHo2M2tsYzZxaTl0c2Z2In0.10op999PW31nBLAx5aMbdg",
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }
+};
+
+export default nextConfig;
